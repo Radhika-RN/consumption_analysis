b963d114f084c0516ab157e9899cc2fde4c03207
Radhika Nair <radhikarnair451@gmail.com>
2022-01-21 16:13:17 +0100

    Added minimal comments

--- gorlib.py
+++ gorlib.py
@@ -16,18 +16,17 @@ def load_file(fpath):
 
 def calc_meter_cost(forecast, rates, mid, ezone, aq, sdate, edate):
     """
+    Calculates the total cost and consumption for a single meter
     """
     fdsel = (forecast["Meter ID"]== mid) & (forecast["Date"] >= sdate) & (forecast["Date"] < edate)
     rsel = (rates['Exit Zone'] == ezone) & (rates['Annual Quantity (Min)'] <= aq) & (rates['Annual Quantity (Max)'] > aq)
     dff = forecast.loc[fdsel]
     dff = dff.set_index('Date')
     dff = dff.sort_index()
-    #Add comments
     dfr = rates.loc[rsel, ['Date','Rate (p/kWh)']]
     dfr = dfr.set_index('Date')
     dfr = dfr.sort_index()
     dfr = dfr.reindex(labels=pd.date_range(dfr.index[0],dfr.index[-1],freq='D'), method='pad')
-    #Add comments
     dfj = dff.join(dfr)
     dfj["Cost"] = dfj['Rate (p/kWh)'] * dfj['kWh']
     return {"Tcost": (dfj["Cost"].sum()/100.0).round(2), "TkWh": dfj["kWh"].sum().round(2)} 
@@ -35,23 +34,25 @@ def calc_meter_cost(forecast, rates, mid, ezone, aq, sdate, edate):
 
 def calc_meter_cost_opt(forecast, rates, mid, ezone, aq, sdate, edate):
     """
+    Same as calc_meter_cost: Used for testing optimization
     """
     fdsel = (forecast["Meter ID"]== mid) & (forecast["Date"] >= sdate) & (forecast["Date"] < edate)
     rsel = (rates['Exit Zone'] == ezone) & (rates['Annual Quantity (Min)'] <= aq) & (rates['Annual Quantity (Max)'] > aq)
     dff = forecast.loc[fdsel]
     dff = dff.set_index('Date')
     dff = dff.sort_index()
-    #Add comments
     dfr = rates.loc[rsel, ['Date','Rate (p/kWh)']]
     dfr = dfr.set_index('Date')
     dfr = dfr.sort_index()
     dfr = dfr.reindex(labels=pd.date_range(dfr.index[0],dfr.index[-1],freq='D'), method='pad')
-    #Add comments
     #dfj = dff.join(dfr)
     dfr["Cost"] = dfr['Rate (p/kWh)'] * dff['kWh']
-    return {"Tcost": (dfr["Cost"].sum()/100.0).round(2), "TkWh": dff["kWh"].sum().round(2)} 
+    return [(dfr["Cost"].sum()/100.0).round(2), dff["kWh"].sum().round(2)]
 
 def calc_costs(dfconsm, dfrates, dfmeters, sdate, edate):
+    """
+    Calculates the total cost and consumption for all meters
+    """
     mlist=[]
     Tcost=[]
     TkWh=[]
@@ -66,20 +67,18 @@ def calc_costs(dfconsm, dfrates, dfmeters, sdate, edate):
     return df 
 
 def calc_costs_opt(dfconsm, dfrates, dfmeters, sdate, edate):
-    mlist=[]
-    Tcost=[]
-    TkWh=[]
-    for index, row in dfmeters.iterrows():
-        res = calc_meter_cost_opt(dfconsm, dfrates, row["Meter ID"], row["Exit Zone"],
-                         row["Annual Quantity (kWh)"], sdate, edate)
-        mlist.append(row["Meter ID"])
-        Tcost.append(res["Tcost"])
-        TkWh.append(res["TkWh"])
-    data = list(zip(mlist,TkWh,Tcost))
-    df = pd.DataFrame(data, columns=["Meter ID","Total Estimated Consumption (kWh)","Total Cost (Pounds)"])
-    return df 
+    """
+    Same as calc_costs: Used for testing optimization
+    """
+    dfmeters[["Total Estimated Consumption (kWh)","Total Cost (Pounds)"]] = dfmeters.apply(lambda row: calc_meter_cost_opt(dfconsm, 
+                                                                           dfrates, row["Meter ID"], row["Exit Zone"],
+                                                                           row["Annual Quantity (kWh)"], sdate, edate), axis=1,result_type="expand")
+    return dfmeters[["Meter ID","Total Estimated Consumption (kWh)","Total Cost (Pounds)"]]
 
 def gen_rand_meters(rates, mcnt=100):
+    """
+    Generates random meter dataframe
+    """
     uexits = rates["Exit Zone"].unique().tolist()
     df = pd.DataFrame()
     df["Meter ID"] = random.sample(range(1000,1000+mcnt),mcnt) 
@@ -88,6 +87,9 @@ def gen_rand_meters(rates, mcnt=100):
     return df
 
 def gen_mock_consn(mlist, sdate, edate):
+    """
+    Generates mock consumption dataframe
+    """
     dflist=[]
     for meter in mlist:
         d_df = pd.DataFrame()
